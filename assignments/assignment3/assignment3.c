5325e4f8547738813a46c540bc5b230e79e65e66
Madeline LeBreton <madeline.lebreton@gmail.com>
2026-03-11 18:35:41 +0000

    Adding fifo implementation and tlb

--- assignments/assignment3/assignment3.c
+++ assignments/assignment3/assignment3.c
@@ -15,30 +15,75 @@
 #define PAGE_SIZE 256 // page size 256 bytes
 #define NUM_PT_ENTRIES (1<<8) // one entry per page, with 16 bit VA - 8 bits for offset = 8 bits to rep. pages
 #define FRAME_COUNT (1<<7) // 15 bit PA, with 8 bit offset leaves 7 bits for frames
+#define TLB_SIZE (1<<4); // transfer lookaside buffer
+
+// tlb structure, simulates cache for page table
+typedef struct {
+    int page;
+    int frame;
+} TLB_Struct;
+
+// global structures
+TLB_Struct tlb[TLB_SIZE]; // transfer lookaside buffer
+int tlb_index = 0;
+
+int pt[NUM_PT_ENTRIES]; // page table, -1 means not in memory
+int frame_table[FRAME_COUNT]; // track which page is in which frame
+
+signed char physical_memory[FRAME_COUNT][PAGE_SIZE]; // need enough memory for 256 byte page x 2^8 pages. each index rep. 1 byte
+int fifo_ptr = 0; 
+int tlb_hits = 0; 
+int page_faults = 0; // counters for validation
+int num_addresses = 0;
+
+
+
+// tlb functions
+int search_tlb(int page){ // search tlb for referenced page number
+  for(int i=0; i<TLB_SIZE; i++){
+    if(tlb[i].page == page)
+      return tlb[i].frame; // return corresponding frame number
+  } return -1; // if the page isn't found, return -1
+}
+
+// add to tlb using first in, first out replacement policy
+void add_TLB(int page, int frame){
+  tlb[tlb_index].page = page;
+  tlb[tlb_index].frame = frame;
+  
+  // increment index for next time, in circular fashion
+  tlb_index = (tlb_index + 1) % TLB_SIZE;
+}
+
+// update tlb when page is replaced
+void update_tlb(int page, int frame){
+  for(int i=0; i<TLB_SIZE; i++){
+    if(tlb[i].page == page){
+      tlb[i].frame = frame;
+    }
+  }
+}
+      
 
 
 // main
 int main(){
-    int pt[NUM_PT_ENTRIES]; // page table
-    signed char physical_memory[1<<15]; // need enough memory for 256 byte page x 2^8 pages. each index rep. 1 byte
-    int next_free_frame = 0; // for FIFO page replacement, simulating oldest page as the victim
-    int page_faults = 0; // counters for validation
-    int num_addresses = 0;
-    int tlb_hits = 0; 
-    
-    FILE *address_file; // pointer to addresses.txt
-    int la; // logical address, will be a 16 bit binary number
     
     // initialize page table
     for(int i=0; i<NUM_PT_ENTRIES; i++){
         pt[i] = -1; // initialize to -1 for "invalid", page not yet in memory
     }
+    
+    // initialize tlb
+    for(int i=0; i<TLB_SIZE; i++){
+      frame_table[i] = -1; // initialize values to -1
+    }
 
     // open backing store. mmap() only works with a file descriptor so we use open() with O_RDONLY
     int backing_fd = open("BACKING_STORE.bin", O_RDONLY); // get file descriptor
-
     if(backing_fd == -1){
         perror("Error opening BACKING_STORE.bin");
+        return 1; // error flag
     }
 
     // memory map the backing store
@@ -49,23 +94,38 @@ int main(){
     }
     
     // read addresses from file
+    FILE *address_file; // pointer to addresses.txt
     address_file = fopen("addresses.txt", "r"); // open the text file with read permissions
     if(address_file == NULL){ // file open failure returns NULL
         printf("Error opening address.txt\n");
         return 1; // error flag
     }
-
+    
+    int la; // logical address, will be a 16 bit binary number
+    
+    // MAIN LOOP
     // extract page number and offset from addresses
     while(fscanf(address_file, "%d", &la) != EOF){ // EOF end of file macro from <stdio.h>
-        int page_num = la >> 8 & 0xFF; // shift right to capture 8 MSB, then mask with binary 11111111 to only keep those bits
+        int page_num = la >> 8; // shift right to capture 8 MSB, then mask with binary 11111111 to only keep those bits
         int offset = la & 0xFF; // mask MSB to capture 8 LSB
 
-        // handling a logical address: lookup in page table, if page not in memory handle page fault
-        int frame_number = pt[page_num];
+        // handling a logical address: lookup in tlb, if miss then page table, if page not in memory handle page fault
+        int frame_number = search_TLB(page);
 
-        if(frame_number == -1){ // invalid, page fault
-            page_faults++;
-
+        if(frame_number == !-1){ // valid, tlb HIT!
+            tlb_hits++;
+        }
+        else{
+          // page table lookup
+          frame = pt[page];
+          
+          // page fault if invalid
+          if(frame == -1){
+            int victim_frame = fifo_ptr;
+            fifo_ptr
+            ************** start coding here
+          
+          
             // copy page from backing store to physical memory
             frame_number = next_free_frame; // the faulted page will reside in the next available frame
             next_free_frame = (next_free_frame +1) % FRAME_COUNT; // increment so the oldest frame is always replaced
