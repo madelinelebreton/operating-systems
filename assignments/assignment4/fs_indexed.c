2f5dbbeb180c18e74e2b542d1031b5f2c9623de8
Madeline LeBreton <madeline.lebreton@gmail.com>
2026-03-27 07:12:56 -0400

    Added header file for file management system

--- assignments/assignment4/fs_indexed.c
+++ assignments/assignment4/fs_indexed.c
@@ -1,1 +1,108 @@
 // fs_indexed.c
+// implements file system functions 
+
+#include <fs_indexed.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// inialize file system (mount operation)
+void initFS(void){
+    // initialize volume control block, metadata
+    fs.totalBlocks = MAX_BLOCKS;
+    fs.blockSize = BLOCK_SIZE;
+
+    // initialize FIB table (directory)
+    fs.fileCount = 0; // empty to start
+
+    // initialize free block list. OS knows which disk blocks availabile, in allcation order
+    int index = 0;
+    for(int i=0; i<MAX_BLOCKS; i++){
+        fs.freeBlocks[index] = i;
+        index++;
+    }
+    fs.freeBlockCount = index; // set counter
+
+    // print success message
+    printf("File system mounted successfully\n");
+    printf("Total blocks: %d, block size %d", fs.totalBlocks, fs.blockSize);
+}
+
+// file operations. mimic system calls
+int createFile(char* filename, int size){
+    
+    // compute required blocks
+    int dataBlocks = (size + BLOCK_SIZE-1)/BLOCK_SIZE; // need to add block size to avoid truncating down (ceiling division). for partial blocks
+    int totalBlocks = 2 + dataBlocks; // also need 1 for FIB and 1 for index block
+
+    // check file limit
+    if(fs.fileCount >= MAX_FILES){ // check that metadata can be stored
+        printf("Error: max file limit reached.\n"); 
+        return;
+    }
+
+    if(totalBlocks > fs.freeBlockCount == 0){ // check that disk has enough space
+        printf("Error: not enough free blocks.\n");
+        return;
+    }
+
+    // allocate FIB first, it's needed to point to the index block (pointer chain FIB > index block > data blocks)
+    int fibBlock = allocateFreeBlock();
+    if(fibBlock == -1){
+        printf("Error allocating FIB block\n");
+        return;
+    }
+
+    // allocate index block (file map)
+    int indexBlock = allocateFreeBlock();
+    if(indexBlock == -1){
+        printf("Error allocating index block.\n");
+        return;
+    }
+    
+    // allocate data blocks
+    int dataBlock;
+
+    for(int i=0; i< dataBlocks; i++){ // sequential file layout
+        dataBlock = allocateFreeBlock();
+        if(dataBlock == -1){
+            printf("Error allocating data block\n");
+            return;
+        }
+        // store pointers in index block array. indexBlock[i] = disk block number
+        fs.indexBlock[indexBlock][i] = dataBlock;
+    }
+
+
+    // update directory
+}
+
+int deleteFile(char* filename);
+void listFiles(void);
+
+// free block operations
+// remove a block from free block list and return it
+int allocateFreeBlock(void){
+    // fs.freeBlocks[] is list of available blocks
+    // fs.freeBlockCount is number of free blocks
+
+    // check if no free blocks available
+    if(fs.freeBlockCount == 0){
+        return -1;
+    }
+    // get a block from end of free block list. constant time allocation without shifting (stack)
+    int block = fs.freeBlocks[fs.freeBlockCount-1];
+    // update free block count
+    fs.freeBlockCount--;
+    // return allocated block
+    return block;
+}
+
+void returnFreeBlock(int block){
+    // return to end of free block list (stack implementation)
+    fs.freeBlockList[fs.freeBlockCount] = block;
+    // increment free block counter
+    fs.freeBlockCount++;
+}
+
+// utility
+void printFreeBlocks(void);
--- labs/lab4/fs_indexed.h
+++ labs/lab4/fs_indexed.h
@@ -2,21 +2,41 @@
 // file system simulator header file
 // contains struct definitions, constants, and function prototypes
 
+// header files
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h> // needed for ceil() with division
+
+
 // system constants
 #define MAX_FILES 10
 #define MAX_BLOCKS 64
 #define MAX_BLOCKS_PER_FILE 8
-#define BLOCK_SIZE 512
+#define BLOCK_SIZE 2^10
+
 
 // global file system instance. simulates single kernel instance
 extern FileSystem fs;
 
-// index block, integer array of all data block numbers. the file uses multiple data blocks
+// index block, integer array of all data blocks allocated to the file
 int indexBlock[MAX_BLOCKS_PER_FILE];
 
 
+// Block represents a fixed-size portion of the disk
+typedef struct Block{
+    unsigned char data[BLOCK_SIZE];
+    int blockNumber;
+} Block;
+
+// node of free block linked list 
+typedef struct FreeBlockNode{
+    int node;
+    struct FreeBlockNode* next;
+} FreeBlockNode;
+
 // File Information Block stores file attributes, permissions, and location of file. one per file
-typedef struct {
+typedef struct FIB{
   char filename[50];
   int fileSize; 
   
@@ -25,16 +45,14 @@ typedef struct {
   int numBlocks; // number of data blocks used
 } FIB;
 
+
+
 // GLOBAL file system, kernel memory structure for OS mapping
-typedef struct {
-  FIB directory[MAX_FILES]; // in-memory directory structure storing all FIBs (flat, single-level)
-  int fileCount; // number of directory entries in use
-  
-  int freeBlocks; // free block list
-  int freeBlockCount; 
-  
-  int totalBlocks; 
-  int blockSize;
+typedef struct FileSystem{
+    Block blocks[MAX_BLOCKS]; // array of all blocks on disk
+    FIB directory[MAX_FILES]; // in-memory directory structure storing all FIBs (flat, single-level)
+    FreeBlockNode* freeList; // linked list of all free blocks for efficient insert/remove
+    int fileCount; // number of directory entries in use
 } FileSystem;
 
 
@@ -43,7 +61,7 @@ typedef struct {
 void initFS(void);
 
 // file operations. mimic system calls
-int createFile(char* filename, int size);
+int createFile(char* filename, int size); // returns success/failure indicator
 int deleteFile(char* filename);
 void listFiles(void);
 
